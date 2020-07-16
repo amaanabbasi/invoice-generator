2bbb2b2ab611d4615dce80b1b3f93e349efdf57b
amaanabbasi <amaanabbasi99@gmail.com>
2020-07-16 12:26:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# invoice-generator.py
+Generate invoices in Python requests library with the Invoice Generator API.
+
+Usage
+
+First, install ```requests```.
+
+```pip install requests```
+
+Second, customize invoice.py with the parameters for your invoice. Then run this to download a PDF:
+
+```python invoice.py```
---
+++ invoice.py
@@ -0,0 +1,34 @@
+import requests
+
+
+url = 'https://invoice-generator.com'
+
+headers = {
+    "logo": "http://invoiced.com/img/logo-invoice.png",
+    "from": "Invoiced\n701 Brazos St\nAustin, TX 78748",
+    "to": "Johnny Appleseed",
+    "currency": "usd",
+    "number": "INV-0001",
+    "payment_terms": "Auto-Billed - Do Not Pay",
+    "items": [
+        {
+            "name": "Subscription to Starter Pro Pack",
+            "quantity": 1,
+            "unit_cost": 16
+        }
+    ],
+    "fields": {
+        "tax": "%"
+    },
+    "tax": 5,
+    "notes": "Thanks for being an awesome customer!",
+    "terms": "No need to submit payment. You will be auto-billed for this invoice."
+}
+
+response = requests.post(url, json = headers)
+
+print("Status code: ", response.status_code)
+print("Printing Entire Post Request")
+
+with open('invoice.pdf', 'wb') as f:
+    f.write(response.content)
